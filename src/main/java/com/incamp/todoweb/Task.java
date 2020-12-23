6d4bbd0ed77b4808c1679d7688368ca80102139e
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-12-23 12:09:32 +0200

    Implement postTask

--- src/main/java/com/incamp/todoweb/Task.java
+++ src/main/java/com/incamp/todoweb/Task.java
@@ -1,5 +1,43 @@
 package com.incamp.todoweb;
 
+import java.time.LocalTime;
+
 public class Task {
-    // TODO
+    private long id;
+    private final String title;
+    private final String description;
+    private final boolean done;
+    private final LocalTime dueTime;
+
+    public Task(long id, String title, String description, boolean done, LocalTime dueTime) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.done = done;
+        this.dueTime = dueTime;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public LocalTime getDueTime() {
+        return dueTime;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/incamp/todoweb/TaskController.java
+++ src/main/java/com/incamp/todoweb/TaskController.java
@@ -1,26 +1,34 @@
 package com.incamp.todoweb;
 
+import java.time.LocalTime;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TaskController {
     private final AtomicLong idCounter = new AtomicLong();
-    private final List<Task> tasks = Arrays.asList(); // FIXME
+    private final List<Task> tasks = Arrays.asList(
+            new Task(
+                    idCounter.getAndIncrement(),
+                    "Task 1",
+                    "Task 1 Description",
+                    false,
+                    LocalTime.of(10, 10))
+    );
 
     @GetMapping("/tasks")
-    public Object[] getTasks() {
-        return tasks.toArray();
+    public Collection<Task> getTasks() {
+        return tasks;
     }
 
     @PostMapping("/tasks")
-    public Task postTask() {
-        return new Task(); // FIXME
+    public Task postTask(@RequestBody Task task) {
+        task.setId(idCounter.getAndIncrement());
+        tasks.add(task);
+        return task;
     }
 }
