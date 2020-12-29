3151e40a0da643048a6358840b26d2a24e0d510c
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-12-29 11:23:29 +0200

    Fix task collection when dueTime is null

--- src/main/java/com/incamp/todoweb/TaskCollectionService.java
+++ src/main/java/com/incamp/todoweb/TaskCollectionService.java
@@ -5,13 +5,23 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskCollectionService {
     @Autowired
     private TaskService taskService;
 
+    // Returns tasks that are not done yet at some date
     public Collection<Task> getTasksForDate(LocalDateTime date) {
-
+        return taskService
+                .getTasks()
+                .stream()
+                .filter(x -> !x.isDone()
+                        && x.getDueTime() != null
+                        && x.getDueTime().getYear() == date.getYear()
+                        && x.getDueTime().getMonth() == date.getMonth()
+                        && x.getDueTime().getDayOfMonth() == date.getDayOfMonth())
+                .collect(Collectors.toList());
     }
 }
