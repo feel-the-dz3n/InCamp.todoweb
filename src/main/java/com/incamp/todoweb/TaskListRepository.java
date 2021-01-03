c94b890cb5ba8a3f62087669240c77707e6ddcd5
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-03 20:55:04 +0200

    Remove comments at TaskListRepository

--- src/main/java/com/incamp/todoweb/TaskListRepository.java
+++ src/main/java/com/incamp/todoweb/TaskListRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Collection;
 
 public interface TaskListRepository extends CrudRepository<TaskList, Integer> {
-    // Get a collection of TaskList.id, TaskList.name and count incomplete tasks for each list
-    // TODO: put normally formatted request into orm.xml
     @Query("select new TaskListInfo(list.id, list.name, sum(case when task.taskList=list and task.done=false then 1 else 0 end) as tasksLeftCount) from Task task right join TaskList list on task.taskList=list group by list.id")
     Collection<TaskListInfo> fetchTaskListInfo();
 }
