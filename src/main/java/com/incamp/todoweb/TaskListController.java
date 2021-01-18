55636faa89f2c724c920dcf9164f3ba85ecc638c
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-18 17:44:40 +0200

    Add request to get task list by id

--- src/main/java/com/incamp/todoweb/TaskListController.java
+++ src/main/java/com/incamp/todoweb/TaskListController.java
@@ -3,6 +3,8 @@ package com.incamp.todoweb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping(path = "/lists")
 @CrossOrigin
@@ -13,5 +15,10 @@ public class TaskListController {
     @GetMapping("/{id}/tasks")
     public Iterable<Task> getListTasks(@PathVariable Integer id, boolean all) {
         return taskListService.getTaskListTasksFiltered(id, all);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<TaskList> getTaskList(@PathVariable Integer id) {
+        return taskListService.getTaskList(id);
     }
 }
